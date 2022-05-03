6830e4ff1911563c907437fd7c3000f89e7d1b51
EricEOL <lopes.eric051@gmail.com>
2022-05-03 14:03:57 -0300

    Mapeamento de entidades com JPA

--- src/main/java/com/ericeol/bankline/api/model/Movement.java
+++ src/main/java/com/ericeol/bankline/api/model/Movement.java
@@ -4,15 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Movement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private LocalDateTime createdAt;
     private String description;
     private Double value;
+
+    @Enumerated(EnumType.STRING)
     private MovementType movementType;
 }
